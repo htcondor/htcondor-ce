9ffccd564b7e05adcd0636657ca46ea3423e49a9
Matyas Selmeci <matyas@cs.wisc.edu>
2015-01-26 10:33:13 -0600

    Fix calling superclass's constructor in ResourceAd

--- src/condor_ce_info_query.py
+++ src/condor_ce_info_query.py
@@ -25,7 +25,7 @@ class ResourceAd(classad.ClassAd):
     """
 
     def __init__(self, ce_ad, catalog_entry):
-        super(self.__class__, self).__init__()
+        classad.ClassAd.__init__(self, '[]')
 
         self.ce_ad         = ce_ad
         self.catalog_entry = catalog_entry
