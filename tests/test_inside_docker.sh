c5453c1e3b88b31e9d02db523eaecbcc6457fad8
Derek Weitzel <djw8605@gmail.com>
2015-12-17 10:28:10 -0600

    Adding testing inside docker

---
+++ tests/test_inside_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ls -l /home
+
+# First, install all the needed packages.
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OSG_VERSION}.noarch.rpm
+yum -y install yum-plugin-priorities
+rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OSG_VERSION}-release-latest.rpm
+yum -y install rpm-build gcc gcc-c++ boost-devel globus-rsl-devel condor-classads-devel cmake
+
+# Prepare the RPM environment
+sudo -u centos mkdir -p /home/centos/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cat > /home/centos/.rpmmacros << 'EOF'
+%dist .osg.el6
+%osg 1
+EOF
+
+cp htcondor-ce/config/htcondor-ce.spec /home/centos/rpmbuild/SPECS
+package_version=`grep Version config/htcondor-ce.spec | awk '{print $2}'`
+pushd htcondor-ce
+git archive --format=tar --prefix=htcondor-ce-${package_version}/ HEAD  | gzip >/home/centos/rpmbuild/SOURCES/htcondor-ce-${package_version}.tar.gz
+popd
+
+sudo -u centos rpmbuild -ba /home/centos/rpmbuild/SPECS/htcondor-ce.spec
