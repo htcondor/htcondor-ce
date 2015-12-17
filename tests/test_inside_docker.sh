1453615eb9392ae575adfa6f0da646c17dc4815b
Derek Weitzel <djw8605@gmail.com>
2015-12-17 10:48:55 -0600

    Fixing non-existant sudo in docker

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -11,16 +11,16 @@ rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OS_VERSION}-release-latest
 yum -y install rpm-build gcc gcc-c++ boost-devel globus-rsl-devel condor-classads-devel cmake git
 
 # Prepare the RPM environment
-sudo -u centos mkdir -p /home/centos/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cat > /home/centos/.rpmmacros << 'EOF'
+centos mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cat >> /etc/rpm/macros.dist << 'EOF'
 %dist .osg.el6
 %osg 1
 EOF
 
-cp htcondor-ce/config/htcondor-ce.spec /home/centos/rpmbuild/SPECS
+cp htcondor-ce/config/htcondor-ce.spec /tmp/rpmbuild/SPECS
 package_version=`grep Version config/htcondor-ce.spec | awk '{print $2}'`
 pushd htcondor-ce
-git archive --format=tar --prefix=htcondor-ce-${package_version}/ HEAD  | gzip >/home/centos/rpmbuild/SOURCES/htcondor-ce-${package_version}.tar.gz
+git archive --format=tar --prefix=htcondor-ce-${package_version}/ HEAD  | gzip >/tmp/rpmbuild/SOURCES/htcondor-ce-${package_version}.tar.gz
 popd
 
-sudo -u centos rpmbuild -ba /home/centos/rpmbuild/SPECS/htcondor-ce.spec
+rpmbuild --buildroot=/tmp/rpmbuild -ba /tmp/rpmbuild/SPECS/htcondor-ce.spec
