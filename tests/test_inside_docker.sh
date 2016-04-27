97452b61c229c34f8643e6b40ccb1a705521de50
Brian Bockelman <bbockelm@cse.unl.edu>
2016-04-27 16:04:27 -0500

    Exclude apparently broken mirror.

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -10,6 +10,10 @@ yum -y clean expire-cache
 
 # First, install all the needed packages.
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
+
+# Broken mirror?
+echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
+
 yum -y install yum-plugin-priorities
 rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OS_VERSION}-release-latest.rpm
 yum -y install rpm-build gcc gcc-c++ boost-devel cmake git tar gzip make autotools
