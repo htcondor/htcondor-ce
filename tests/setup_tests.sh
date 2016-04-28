dcdf224e1b2cfea818e413d032166592b7a8c692
Derek Weitzel <djw8605@gmail.com>
2016-04-28 09:27:10 -0500

    Adding -xe to bash command for testing (#93)
    * Adding -xe to bash command for testing
    
    Adding the `-xe` command for testing so that if any of the command exits with non-zero status, all of it will exit.
    
    * Remove debugging errors.

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -15,11 +15,8 @@ elif [ "$el_version" = "7" ]; then
 docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -c "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION};
-  echo -ne \"------\nEND HTCONDOR-CE TESTS\n------\nSystemD Units:\n------\n\"; 
-  systemctl --no-pager --all --full status;
-  echo -ne \"------\nJournalD Logs:\n------\n\" ;
-  journalctl --catalog --all --full --no-pager;"
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION};
+  echo -ne \"------\nEND HTCONDOR-CE TESTS\n\";"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
 docker rm -v $DOCKER_CONTAINER_ID
