92e7d878f59333de72dd0e33ee8c8ff846ed1ffa
Brian Lin <brianhlin@gmail.com>
2018-03-08 10:33:24 -0600

    Scripts inherit the env so we don't need to pass them explicitly

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -2,16 +2,13 @@
 
 # This script starts docker and systemd (if el7)
 
-# Version of CentOS/RHEL
-el_version=$1
-
 # Run tests in Container
 # We use `--privileged` for cgroup compatability, which seems to be enabled by default in HTCondor 8.6.x
-if [ "$el_version" = "6" ]; then
+if [ "${OS_VERSION}" = "6" ]; then
 
 sudo docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup --rm=true -v `pwd`:/htcondor-ce:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION}"
 
-elif [ "$el_version" = "7" ]; then
+elif [ "${OS_VERSION}" = "7" ]; then
 
 docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
