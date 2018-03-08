e1dbef592fb19292e0f2d1d5c7f9e300a7102d7a
Brian Lin <brianhlin@gmail.com>
2018-03-08 10:54:21 -0600

    Improve readability of docker setup tests

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -6,18 +6,29 @@
 # We use `--privileged` for cgroup compatability, which seems to be enabled by default in HTCondor 8.6.x
 if [ "${OS_VERSION}" = "6" ]; then
 
-sudo docker run --privileged -v /sys/fs/cgroup:/sys/fs/cgroup --rm=true -v `pwd`:/htcondor-ce:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION} ${BUILD_ENV}"
+    sudo docker run --privileged --rm=true \
+         --volume /sys/fs/cgroup:/sys/fs/cgroup \
+         --volume `pwd`:/htcondor-ce:rw \
+         centos:centos${OS_VERSION} \
+         /bin/bash -c "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION} ${BUILD_ENV}"
 
 elif [ "${OS_VERSION}" = "7" ]; then
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
-DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION} ${BUILD_ENV};
-  echo -ne \"------\nEND HTCONDOR-CE TESTS\n\";"
-docker ps -a
-docker stop $DOCKER_CONTAINER_ID
-docker rm -v $DOCKER_CONTAINER_ID
+    docker run --privileged --detach --tty --interactive --env "container=docker" \
+           --volume /sys/fs/cgroup:/sys/fs/cgroup \
+           --volume `pwd`:/htcondor-ce:rw  \
+           centos:centos${OS_VERSION} \
+           /usr/sbin/init
+
+    DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+    docker logs $DOCKER_CONTAINER_ID
+    docker exec --tty --interactive $DOCKER_CONTAINER_ID \
+           /bin/bash -xec "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION} ${BUILD_ENV};
+           echo -ne \"------\nEND HTCONDOR-CE TESTS\n\";"
+
+    docker ps -a
+    docker stop $DOCKER_CONTAINER_ID
+    docker rm -v $DOCKER_CONTAINER_ID
 
 fi
 
