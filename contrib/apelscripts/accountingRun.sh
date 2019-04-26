c613d7fc1f697bad4c4f4a010f48ec3b4254d9e5
sjones-hep-ph-liv-ac-uk <sjones@hep.ph.liv.ac.uk>
2019-04-26 14:21:00 +0100

    Move from /usr/share to /usr/bin

--- contrib/apelscripts/accountingRun.sh
+++ contrib/apelscripts/accountingRun.sh
@@ -3,8 +3,8 @@
 # sjones@hep.ph.liv.ac.uk, 2019
 # Run the processes of a HTCondor accounting run
 
-/usr/share/apel/condor_ce_blah.sh       # Make the blah file (CE/Security data)
-/usr/share/apel/condor_ce_batch.sh      # Make the batch file (Job times)
+/usr/bin/condor_ce_blah.sh         # Make the blah file (CE/Security data)
+/usr/bin/condor_ce_batch.sh        # Make the batch file (Job times)
 /usr/bin/apelparser                # Read the blah and batch files in
 /usr/bin/apelclient                # Join blah and batch records to make job records
 /usr/bin/ssmsend                   # Send job records into APEL system
