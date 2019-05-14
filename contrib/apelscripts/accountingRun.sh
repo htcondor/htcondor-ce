d67bad4ce4c67d01a35767c9da77595eb92eacbc
sjones-hep-ph-liv-ac-uk <sjones@hep.ph.liv.ac.uk>
2019-05-14 14:29:13 +0100

    Match the new name for the batch extraction script

--- contrib/apelscripts/accountingRun.sh
+++ contrib/apelscripts/accountingRun.sh
@@ -11,7 +11,7 @@ if [ -r  $default ]; then
 fi
 
 /usr/bin/condor_ce_blah.sh  $conf  # Make the blah file (CE/Security data)
-/usr/bin/condor_ce_batch.sh $conf  # Make the batch file (Job times)
+/usr/bin/condor_batch.sh    $conf  # Make the batch file (Job times)
 /usr/bin/apelparser                # Read the blah and batch files in
 /usr/bin/apelclient                # Join blah and batch records to make job records
 /usr/bin/ssmsend                   # Send job records into APEL system
