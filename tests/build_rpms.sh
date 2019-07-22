57854406bc2b2fda73381b3cb40b5bca08183195
Matyas Selmeci <matyas@cs.wisc.edu>
2019-07-22 10:52:42 -0500

    WIP: Start work on sftp-based uploader

--- tests/build_rpms.sh
+++ tests/build_rpms.sh
@@ -4,9 +4,73 @@
 # SRPMs will be /tmp/rpmbuild/SRPMS/*.rpm.
 # Binary RPMs will be /tmp/rpmbuild/RPMS/*/*.rpm.
 
-OS_VERSION=${1:-$OS_VERSION}
-BUILD_ENV=${2:-$BUILD_ENV}
-DEPLOY_STAGE=${3:-$DEPLOY_STAGE}
+export OS_VERSION=${1:-$OS_VERSION}
+export BUILD_ENV=${2:-$BUILD_ENV}
+export DEPLOY_STAGE=${3:-$DEPLOY_STAGE}
+export REPO_OWNER=${4:-$REPO_OWNER}
+
+
+if $DEPLOY_STAGE && [[ -z $REPO_OWNER ]]; then
+    echo >&2 "Deploying requires a REPO_OWNER"
+    exit 1
+fi
+
+
+keyfile=$(dirname "$0")/id_rsa_cibot
+upload_server=ci-xfer.chtc.wisc.edu
+# from "ssh-keyscan -t rsa ci-xfer.chtc.wisc.edu"
+hostsig="ci-xfer.chtc.wisc.edu ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyrceRMLPsOmdtDHxXpfI82snDF0Q9/Z1Mick5zsQK1RyOtNgkyvXM50AJSPPSl0I9JmIxSBxhqcNDcbDz0Kc8tKcA1iGQxp4Ll9z9ZCl60AUq72WwkS1A4z11JjRoYvw1CL8bvoJhk55dcgAz+bXWx/eTwcBsmW80/okNDkdYmtv+QgfUmRP6TjMtIkzvCsXi5x+B4j66yQcLDDYb36EcGyHZqoyLuxkxX0OwS7LuzDfnKxpsV9jlnu3PuJnZOizalqKUpTYc2b83XnfsIYTqoiclmFr89+WuQJG6e/596y/9aVtNacCphdS7u3D+tSoME6OG7xQtZiQfkWvKPicv"
+
+
+
+function setup_ssh {
+    if [[ ! -e $keyfile.enc.$REPO_OWNER ]]; then
+        echo "Repo owner $REPO_OWNER does not have a key in the repo." >&2
+        echo "Cannot deploy via ssh." >&2
+        return 1
+    fi
+    (
+        umask 077
+        mkdir -p ~/.ssh
+        openssl aes-256-cbc \
+            -K $encrypted_e14a22ad945b_key \
+            -iv $encrypted_e14a22ad945b_iv \
+            -in $keyfile.enc.$REPO_OWNER -out $keyfile \
+            -d
+        cat > ~/.ssh/config <<__END__
+Host $upload_server
+User cibot
+IdentityFile $keyfile
+PubkeyAuthentication yes
+PasswordAuthentication no
+GSSAPIAuthentication no
+ChallengeResponseAuthentication no
+KerberosAuthentication no
+IdentitiesOnly yes
+__END__
+        printf "%s\n" "$hostsig" > ~/.ssh/known_hosts
+    )
+}
+
+remote_dir=/var/tmp/travis/htcondor-ce/${REPO_OWNER}-${TRAVIS_JOB_NUMBER:-0.0}
+function sftp_to_chtc {
+    local ret=0
+    script=$(mktemp -t build_rpms.$$.XXXXXX)
+    cat >>"$script" <<__END__
+-MKDIR /var/tmp/travis
+-MKDIR /var/tmp/travis/htcondor-ce
+-MKDIR $remote_dir
+CD $remote_dir
+__END__
+
+    for file; do
+        printf 'PUT "%s"\n' "$file" >>"$script"
+    done
+
+    sftp -b "$script" "$upload_server"; ret=$?
+
+    return $ret
+}
 
 
 # Clean the yum cache
@@ -19,7 +83,7 @@ rpm -U https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.n
 # Broken mirror?
 echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
 
-yum -y -d0 install yum-plugin-priorities rpm-build gcc gcc-c++ boost-devel cmake git tar gzip make autotools
+yum -y -d0 install yum-plugin-priorities rpm-build gcc gcc-c++ boost-devel cmake git tar gzip make autotools openssl
 
 if [[ $BUILD_ENV == osg ]]; then
     rpm -U https://repo.opensciencegrid.org/osg/3.4/osg-3.4-el${OS_VERSION}-release-latest.rpm
@@ -67,5 +131,8 @@ if $DEPLOY_STAGE; then
     if [[ $OS_VERSION == 6 ]]; then
         cp -f /tmp/rpmbuild/SRPMS/*.rpm htcondor-ce/travis_deploy/
     fi
+
+    setup_ssh
+    sftp_to_chtc htcondor-ce/travis_deploy/*
 fi
 
