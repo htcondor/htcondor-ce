c62302f2ed60373741e6731145a8ac249b6b35db
Brian Lin <blin@cs.wisc.edu>
2019-09-10 09:40:54 -0500

    pylint fixups

--- src/verify_ce_config.py
+++ src/verify_ce_config.py
@@ -16,15 +16,16 @@ def error(msg):
 def warn(msg):
     print "WARNING: " + msg
 
+
 def condorpp(param):
-    syntax = [';','[',']']
-    out = "".join(param.split()) # strip all whitespace
-    for elem in syntax: 
-      out = out.replace(elem, elem+'\n') # re-print 
+    syntax = [';', '[', ']']
+    out = "".join(param.split())  # strip all whitespace
+    for elem in syntax:
+        out = out.replace(elem, elem+'\n')  # re-print
     return out
 
-def malformedQueues(parsedAds, unparsedAds):
 
+def malformedQueues(parsedAds, unparsedAds):
     allqueues = []
     for line in condorpp(unparsedAds).split('\n'):
         if 'name' in line:
@@ -36,13 +37,15 @@ def malformedQueues(parsedAds, unparsedAds):
 
     return list(set(allqueues) - set(goodqueues))
 
+
+
 # Verify that the HTCondor Python bindings are in the PYTHONPATH
 try:
     import classad
     import htcondor
 except ImportError:
     error("Could not load HTCondor Python bindings. "
-          "Please ensure that the 'htcondor' and 'classad' are in your PYTHONPATH")
+          + "Please ensure that the 'htcondor' and 'classad' are in your PYTHONPATH")
 
 is_osg = htcondor.param.get('OSG_CONFIGURE_PRESENT', '').lower() in ('true', 'yes', '1')
 
@@ -53,7 +56,7 @@ for attr in ['JOB_ROUTER_DEFAULTS', 'JOB_ROUTER_ENTRIES']:
         ads = classad.parseAds(htcondor.param[attr])
     except KeyError:
         continue
-    JOB_ROUTER_CONFIG[attr] = list(ads) # store the ads (iterating through ClassAdStringIterator consumes them)
+    JOB_ROUTER_CONFIG[attr] = list(ads)  # store the ads (iterating through ClassAdStringIterator consumes them)
 
 # Verify job routes. classad.parseAds() ignores malformed ads so we have to compare the unparsed string to the
 # parsed string, counting the number of ads by proxy: the number of opening square brackets, "["
@@ -76,27 +79,28 @@ for entry in JOB_ROUTER_CONFIG['JOB_ROUTER_ENTRIES']:
     overriden_attr = EVAL_SET_DEFAULTS.intersection(set(entry.keys()))
     if overriden_attr:
         warn("%s in JOB_ROUTER_ENTRIES will be overriden by the JOB_ROUTER_DEFAULTS."
-             " Use the 'eval_set_' prefix instead." % ', '.join(overriden_attr))
+             % ', '.join(overriden_attr)
+             + " Use the 'eval_set_' prefix instead.")
 
     # Ensure that users don't set the job environment in the Job Router
     if is_osg and any(x.endswith('environment') for x in entry.keys()):
         error("Do not use the Job Router to set the environment. Place variables under "
-              "[Local Settings] in /etc/osg/config.d/40-localsettings.ini")
+              + "[Local Settings] in /etc/osg/config.d/40-localsettings.ini")
 
     # Warn users about eval_set_ default attributes in the ENTRIES since their
     # evaluation may occur after the eval_set_ expressions containg them in the
     # JOB_ROUTER_DEFAULTS
     no_effect_attr = DEFAULT_ATTR.intersection(set([x for x in entry.keys() if x.startswith('eval_set_')]))
     if no_effect_attr:
-        warn("%s in JOB_ROUTER_ENTRIES may not have any effect. Use the 'set_ prefix instead."
-             % ', '.join(no_effect_attr))
+        warn("%s in JOB_ROUTER_ENTRIES " % ', '.join(no_effect_attr)
+             + "may not have any effect. Use the 'set_' prefix instead.")
 
 # Warn users on OSG CEs if osg-configure has not been run
 if is_osg:
     try:
         htcondor.param['OSG_CONFIGURED']
     except KeyError:
-        warn("osg-configure has not been run, degrading the functionality "
+        warn("osg-configure has not been run, degrading the functionality " +
              "of the CE. Please run 'osg-configure -c' and restart condor-ce.")
 
 # Ensure that HTCondor back-ends have QUEUE_SUPER_USER_MAY_IMPERSONATE set correctly
@@ -110,6 +114,6 @@ else:
     su_attr = 'QUEUE_SUPER_USER_MAY_IMPERSONATE'
     if htcondor.param.get(su_attr, '') != '.*':
         error("HTCondor batch system is improperly configured for use with HTCondor CE. "
-              "Please verify that '%s = .*' is set in your HTCondor configuration." % su_attr)
+              + "Please verify that '%s = .*' is set in your HTCondor configuration." % su_attr)
 finally:
     os.environ['CONDOR_CONFIG'] = '/etc/condor-ce/condor_config'
