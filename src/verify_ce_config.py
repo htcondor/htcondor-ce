7a126865ae59937a5549ee8fefc4b705f379cd09
Brian Lin <blin@cs.wisc.edu>
2020-04-15 17:22:08 -0500

    Add verify_ce_config unit tests

--- src/verify_ce_config.py
+++ src/verify_ce_config.py
@@ -44,100 +44,95 @@ except ImportError:
           + "Please ensure that the 'htcondor' and 'classad' are in your PYTHONPATH")
 
 
-is_osg = htcondor.param.get('OSG_CONFIGURE_PRESENT', '').lower() in ('true', 'yes', '1')
+def main():
+    is_osg = htcondor.param.get('OSG_CONFIGURE_PRESENT', '').lower() in ('true', 'yes', '1')
 
-# Create dict whose values are lists of ads specified in the relevant JOB_ROUTER_* variables
-JOB_ROUTER_CONFIG = {}
-for attr in ['JOB_ROUTER_DEFAULTS', 'JOB_ROUTER_ENTRIES']:
+    # Create dict whose values are lists of ads specified in the relevant JOB_ROUTER_* variables
+    JOB_ROUTER_CONFIG = {}
+    for attr in ['JOB_ROUTER_DEFAULTS', 'JOB_ROUTER_ENTRIES']:
+        try:
+            config_val = htcondor.param[attr]
+        except KeyError:
+            error("Missing required %s configuration value" % attr)
+
+        # store the ads (iterating through ClassAdStringIterator consumes them)
+        JOB_ROUTER_CONFIG[attr] = list(classad.parseAds(config_val))
+
+        # If JRD or JRE can't be parsed, the job router can't function
+        if not JOB_ROUTER_CONFIG[attr]:
+            error("Could not read %s in the HTCondor-CE configuration." % attr)
+
+        if attr == "JOB_ROUTER_ENTRIES":
+            # Warn about routes we can find in the config that don't result in valid ads
+            malformed_entry_names = malformed_entries(config_val)
+            if malformed_entry_names:
+                warn("Could not read JOB_ROUTER_ENTRIES in the HTCondor-CE configuration. " +
+                     "Failed to parse the following routes: %s"
+                     % ', '.join(malformed_entry_names))
+
+            # Warn about routes specified by JOB_ROUTER_ROUTE_NAMES that don't appear in the parsed JRE.
+            # The job router can function this way but it's likely a config error
+            route_order = htcondor.param.get('JOB_ROUTER_ROUTE_NAMES', '')
+            if route_order:
+                missing_route_def = set(route_order).difference(set(parsed_route_names(config_val)))
+                if missing_route_def:
+                    warn("The following are specified in JOB_ROUTER_ROUTE_NAMES "
+                         "but cannot be found in JOB_ROUTER_ENTRIES: %s"
+                         % ', '.join(missing_route_def))
+
+    # Find all eval_set_ attributes in the JOB_ROUTER_DEFAULTS
+    EVAL_SET_DEFAULTS = set([x.lstrip('eval_') for x in JOB_ROUTER_CONFIG['JOB_ROUTER_DEFAULTS'][0].keys()
+                             if x.startswith('eval_set_')])
+
+    # Find all default_ attributes used in expressions in the JOB_ROUTER_DEFAULTS
+    DEFAULT_ATTR = set([re.sub(r'.*(default_\w*).*', 'eval_set_\\1', str(x))
+                        for x in JOB_ROUTER_CONFIG['JOB_ROUTER_DEFAULTS'][0].values()
+                        if isinstance(x, classad.ExprTree) and str(x).find('default_') != -1])
+
+    for entry in JOB_ROUTER_CONFIG['JOB_ROUTER_ENTRIES']:
+        # Warn users if they've set_ attributes that would be overriden by eval_set in the JOB_ROUTER_DEFAULTS
+        overriden_attr = EVAL_SET_DEFAULTS.intersection(set(entry.keys()))
+        if overriden_attr:
+            warn("%s in JOB_ROUTER_ENTRIES will be overriden by the JOB_ROUTER_DEFAULTS."
+                 % ', '.join(overriden_attr)
+                 + " Use the 'eval_set_' prefix instead.")
+
+        # Ensure that users don't set the job environment in the Job Router
+        if is_osg and any(x.endswith('environment') for x in entry.keys()):
+            error("Do not use the Job Router to set the environment. Place variables under "
+                  + "[Local Settings] in /etc/osg/config.d/40-localsettings.ini")
+
+        # Warn users about eval_set_ default attributes in the ENTRIES since their
+        # evaluation may occur after the eval_set_ expressions containg them in the
+        # JOB_ROUTER_DEFAULTS
+        no_effect_attr = DEFAULT_ATTR.intersection(set([x for x in entry.keys() if x.startswith('eval_set_')]))
+        if no_effect_attr:
+            warn("%s in JOB_ROUTER_ENTRIES " % ', '.join(no_effect_attr)
+                 + "may not have any effect. Use the 'set_' prefix instead.")
+
+    # Warn users on OSG CEs if osg-configure has not been run
+    if is_osg:
+        try:
+            htcondor.param['OSG_CONFIGURED']
+        except KeyError:
+            warn("osg-configure has not been run, degrading the functionality " +
+                 "of the CE. Please run 'osg-configure -c' and restart condor-ce.")
+
+    # Ensure that HTCondor back-ends have QUEUE_SUPER_USER_MAY_IMPERSONATE set correctly
     try:
-        config_val = htcondor.param[attr]
+        htcondor.param['JOB_ROUTER_SCHEDD2_NAME']
     except KeyError:
-        error("Missing required %s configuration value" % attr)
+        pass
+    else:
+        os.environ['CONDOR_CONFIG'] = '/etc/condor/condor_config'
+        htcondor.reload_config()
+        su_attr = 'QUEUE_SUPER_USER_MAY_IMPERSONATE'
+        if htcondor.param.get(su_attr, '') != '.*':
+            error("HTCondor batch system is improperly configured for use with HTCondor CE. "
+                  + "Please verify that '%s = .*' is set in your HTCondor configuration." % su_attr)
+    finally:
+        os.environ['CONDOR_CONFIG'] = '/etc/condor-ce/condor_config'
 
-    # store the ads (iterating through ClassAdStringIterator consumes them)
-    JOB_ROUTER_CONFIG[attr] = list(classad.parseAds(config_val))
 
-    # If JRD or JRE can't be parsed, the job router can't function
-    if not JOB_ROUTER_CONFIG[attr]:
-        error("Could not read %s in the HTCondor-CE configuration." % attr)
-
-    if attr == "JOB_ROUTER_ENTRIES":
-        # Warn about routes we can find in the config that don't result in valid ads
-        malformed_entry_names = malformed_entries(config_val)
-        if malformed_entry_names:
-            warn("Could not read JOB_ROUTER_ENTRIES in the HTCondor-CE configuration. " +
-                 "Failed to parse the following routes: %s"
-                 % ', '.join(malformed_entry_names))
-
-        # Warn about routes specified by JOB_ROUTER_ROUTE_NAMES that don't appear in the parsed JRE.
-        # The job router can function this way but it's likely a config error
-        route_order = htcondor.param.get('JOB_ROUTER_ROUTE_NAMES', '')
-        if route_order:
-            missing_route_def = set(route_order).difference(set(parsed_route_names(config_val)))
-            if missing_route_def:
-                warn("The following are specified in JOB_ROUTER_ROUTE_NAMES "
-                     "but cannot be found in JOB_ROUTER_ENTRIES: %s"
-                     % ', '.join(missing_route_def))
-
-# Warn about routes specified by JOB_ROUTER_ROUTE_NAMES that don't appear in the parsed JRE.
-# The job router can function this way but it's likely a config error
-route_order = htcondor.param.get('JOB_ROUTER_ROUTE_NAMES', '')
-route_names = parsed_route_names(JOB_ROUTER_CONFIG['JOB_ROUTER_ENTRIES'])
-if route_order and set(route_order):
-    missing_route_def = set(route_order).difference(set(route_names))
-    if missing_route_def:
-        warn("The following are specified in JOB_ROUTER_ROUTE_NAMES but cannot be found in JOB_ROUTER_ENTRIES: %s"
-             % ', '.join(missing_route_def))
-
-# Find all eval_set_ attributes in the JOB_ROUTER_DEFAULTS
-EVAL_SET_DEFAULTS = set([x.lstrip('eval_') for x in JOB_ROUTER_CONFIG['JOB_ROUTER_DEFAULTS'][0].keys()
-                         if x.startswith('eval_set_')])
-
-# Find all default_ attributes used in expressions in the JOB_ROUTER_DEFAULTS
-DEFAULT_ATTR = set([re.sub(r'.*(default_\w*).*', 'eval_set_\\1', str(x))
-                    for x in JOB_ROUTER_CONFIG['JOB_ROUTER_DEFAULTS'][0].values()
-                    if isinstance(x, classad.ExprTree) and str(x).find('default_') != -1])
-
-for entry in JOB_ROUTER_CONFIG['JOB_ROUTER_ENTRIES']:
-    # Warn users if they've set_ attributes that would be overriden by eval_set in the JOB_ROUTER_DEFAULTS
-    overriden_attr = EVAL_SET_DEFAULTS.intersection(set(entry.keys()))
-    if overriden_attr:
-        warn("%s in JOB_ROUTER_ENTRIES will be overriden by the JOB_ROUTER_DEFAULTS."
-             % ', '.join(overriden_attr)
-             + " Use the 'eval_set_' prefix instead.")
-
-    # Ensure that users don't set the job environment in the Job Router
-    if is_osg and any(x.endswith('environment') for x in entry.keys()):
-        error("Do not use the Job Router to set the environment. Place variables under "
-              + "[Local Settings] in /etc/osg/config.d/40-localsettings.ini")
-
-    # Warn users about eval_set_ default attributes in the ENTRIES since their
-    # evaluation may occur after the eval_set_ expressions containg them in the
-    # JOB_ROUTER_DEFAULTS
-    no_effect_attr = DEFAULT_ATTR.intersection(set([x for x in entry.keys() if x.startswith('eval_set_')]))
-    if no_effect_attr:
-        warn("%s in JOB_ROUTER_ENTRIES " % ', '.join(no_effect_attr)
-             + "may not have any effect. Use the 'set_' prefix instead.")
-
-# Warn users on OSG CEs if osg-configure has not been run
-if is_osg:
-    try:
-        htcondor.param['OSG_CONFIGURED']
-    except KeyError:
-        warn("osg-configure has not been run, degrading the functionality " +
-             "of the CE. Please run 'osg-configure -c' and restart condor-ce.")
-
-# Ensure that HTCondor back-ends have QUEUE_SUPER_USER_MAY_IMPERSONATE set correctly
-try:
-    htcondor.param['JOB_ROUTER_SCHEDD2_NAME']
-except KeyError:
-    pass
-else:
-    os.environ['CONDOR_CONFIG'] = '/etc/condor/condor_config'
-    htcondor.reload_config()
-    su_attr = 'QUEUE_SUPER_USER_MAY_IMPERSONATE'
-    if htcondor.param.get(su_attr, '') != '.*':
-        error("HTCondor batch system is improperly configured for use with HTCondor CE. "
-              + "Please verify that '%s = .*' is set in your HTCondor configuration." % su_attr)
-finally:
-    os.environ['CONDOR_CONFIG'] = '/etc/condor-ce/condor_config'
+if __name__ == "__main__":
+    main()
