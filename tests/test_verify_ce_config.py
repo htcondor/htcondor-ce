e7b15b14ea8ad6ee74af3682e3aea5d7d5bacb96
Brian Lin <blin@cs.wisc.edu>
2020-04-16 11:56:16 -0500

    Make function names verb-based

--- tests/test_verify_ce_config.py
+++ tests/test_verify_ce_config.py
@@ -42,25 +42,25 @@ verify = imp.load_source('verify_ce_config', VERIFY_PATH)
 class TestVerifyConfig(unittest.TestCase):
     """Unit tests for verify_ce_config.py"""
 
-    def test_good_parsed_route_names(self):
+    def test_parse_good_route_names(self):
         """Verify that all routes show up in properly formatted JOB_ROUTER_ENTRIES
         """
-        self.assertEqual(verify.parsed_route_names(GOOD_ROUTES), ['GoodRoute1', 'GoodRoute2'])
+        self.assertEqual(verify.parse_route_names(GOOD_ROUTES), ['GoodRoute1', 'GoodRoute2'])
 
-    def test_bad_parsed_route_names(self):
+    def test_parse_bad_route_names(self):
         """Verify that only properly formatted routes in a JOB_ROUTER_ENTRIES containing bad routes
         """
-        self.assertEqual(verify.parsed_route_names(BAD_ROUTES), ['GoodRoute'])
+        self.assertEqual(verify.parse_route_names(BAD_ROUTES), ['GoodRoute'])
 
-    def test_good_malformed_entries(self):
+    def test_find_good_malformed_entries(self):
         """Verify that no malformed entries are found for properly formatted JOB_ROUTER_ENTRIES
         """
-        self.assertEqual(verify.malformed_entries(GOOD_ROUTES), set([]))
+        self.assertEqual(verify.find_malformed_entries(GOOD_ROUTES), set([]))
 
-    def test_bad_malformed_entries(self):
+    def test_find_bad_malformed_entries(self):
         """Verify that route names are found for malformed JOB_ROUTER_ENTRIES
         """
-        malformed_entry_names = verify.malformed_entries(BAD_ROUTES)
+        malformed_entry_names = verify.find_malformed_entries(BAD_ROUTES)
         self.assertEqual(malformed_entry_names,
                          set(['MissingSemicolon',
                               'SingleQuotedValue',
