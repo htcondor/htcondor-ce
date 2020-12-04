5b83064040e04c5bb99584389b512146ea5d4897
Brian Lin <blin@cs.wisc.edu>
2020-12-04 13:49:09 -0600

    Remove required CE View packages from pip
    We need all requirements to be be brought in through RPMs

--- tests/build_rpms.sh
+++ tests/build_rpms.sh
@@ -88,13 +88,3 @@ rpmbuild --define '_topdir /tmp/rpmbuild' -bb /tmp/rpmbuild/SPECS/htcondor-ce.sp
 mkdir -p htcondor-ce/travis_deploy
 cp -f /tmp/rpmbuild/RPMS/*/*.rpm htcondor-ce/travis_deploy/
 cp -f /tmp/rpmbuild/SRPMS/*.rpm htcondor-ce/travis_deploy/
-
-# Install the python3 rrdtool manually, must be compiled from source to import correctly
-git clone -q https://github.com/commx/python-rrdtool
-pushd python-rrdtool
-python3 setup.py install
-popd
-
-# Manually install the required python3 packages
-# TODO: These should be getting installed from the requirements.txt file.
-pip3 install flask genshi CherryPy==3.*
