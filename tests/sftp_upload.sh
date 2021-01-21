5f5567850798e52bac8582a6eebf4fca61ea1f83
Brian Lin <blin@cs.wisc.edu>
2021-01-21 15:52:22 -0600

    Move SSH key handling to ssh-agent outside of the script

--- tests/sftp_upload.sh
+++ tests/sftp_upload.sh
@@ -17,7 +17,6 @@ fi
 project=${GITHUB_REPOSITORY#*/}
 repo_owner=${GITHUB_REPOSITORY%/*}
 
-keyfile=$progdir/id_rsa_cibot2
 upload_server=ci-xfer.chtc.wisc.edu
 # from "ssh-keyscan -t rsa ci-xfer.chtc.wisc.edu"
 hostsig="ci-xfer.chtc.wisc.edu ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyrceRMLPsOmdtDHxXpfI82snDF0Q9/Z1Mick5zsQK1RyOtNgkyvXM50AJSPPSl0I9JmIxSBxhqcNDcbDz0Kc8tKcA1iGQxp4Ll9z9ZCl60AUq72WwkS1A4z11JjRoYvw1CL8bvoJhk55dcgAz+bXWx/eTwcBsmW80/okNDkdYmtv+QgfUmRP6TjMtIkzvCsXi5x+B4j66yQcLDDYb36EcGyHZqoyLuxkxX0OwS7LuzDfnKxpsV9jlnu3PuJnZOizalqKUpTYc2b83XnfsIYTqoiclmFr89+WuQJG6e/596y/9aVtNacCphdS7u3D+tSoME6OG7xQtZiQfkWvKPicv"
@@ -31,7 +30,6 @@ function setup_ssh_to_chtc {
         cat > ~/.ssh/config <<__END__
 Host $upload_server
 User cibot
-IdentityFile $keyfile
 PubkeyAuthentication yes
 PasswordAuthentication no
 GSSAPIAuthentication no
@@ -39,7 +37,6 @@ ChallengeResponseAuthentication no
 KerberosAuthentication no
 IdentitiesOnly yes
 __END__
-        printf "%s\n" "${CI_XFER_KEY}" > ~/.ssh/id_rsa_cibot2
         printf "%s\n" "$hostsig" > ~/.ssh/known_hosts
     )
 }
