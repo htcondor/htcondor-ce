d57c16daae669cfb01c7a7427457ff2709e9930a
David Rebatto <david.rebatto@mi.infn.it>
2021-03-11 11:21:21 +0100

    Use a better name for the knob. Also, include missing 'fail' function.

--- contrib/apelscripts/condor_ce_apel.sh
+++ contrib/apelscripts/condor_ce_apel.sh
@@ -2,6 +2,11 @@
 # sjones@hep.ph.liv.ac.uk, 2019
 # Run the processes of an HTCondor-CE + HTCondor accounting run
 
+fail () {
+    echo "$@" >&2
+    exit 1
+}
+
 safe_config_val () {
     var=$1
     attr=$2
@@ -10,12 +15,12 @@ safe_config_val () {
     eval "$var"='$val'
 }
 
-safe_config_val USE_PUBLISHER APEL_USE_PUBLISHER
+safe_config_val SEND_RECORDS APEL_SEND_RECORDS
 
 /usr/share/condor-ce/condor_blah.sh       # Make the blah file (CE/Security data)
 /usr/share/condor-ce/condor_batch.sh      # Make the batch file (batch system job run times)
 /usr/bin/apelparser                       # Read the blah and batch files in
-if [ "$USE_PUBLISHER" == "False"]; then
+if [ "$SEND_RECORDS" == "True" ]; then
     /usr/bin/apelclient                   # Join blah and batch records to make job records
     /usr/bin/ssmsend                      # Send job records into APEL system
 fi
