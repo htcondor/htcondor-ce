aeb98b955c846976c36e4cf48c7a04f4871cf74e
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-03-26 12:57:59 -0500

    APEL reporting script updates per code review (HTCONDOR-293)

--- contrib/apelscripts/condor_batch_blah.sh
+++ contrib/apelscripts/condor_batch_blah.sh
@@ -8,38 +8,46 @@ fail () {
 safe_config_val () {
     var=$1
     attr=$2
+    val=$(condor_config_val $attr) ||
+    fail "Failed to retrieve Condor configuration value '$attr'"
+    eval "$var"='$val'
+}
+
+safe_ce_config_val () {
+    var=$1
+    attr=$2
     val=$(condor_ce_config_val $attr) ||
     fail "Failed to retrieve CE configuration value '$attr'"
     eval "$var"='$val'
 }
 
-OUTPUT_DIR="$(condor_ce_config_val APEL_OUTPUT_DIR)"
+safe_ce_config_val OUTPUT_DIR APEL_OUTPUT_DIR
 [[ -d $OUTPUT_DIR && -w $OUTPUT_DIR ]] || fail "Cannot write to $OUTPUT_DIR"
 OUTPUT_DATETIME=`date '+%Y%m%d-%H%M'`
 
-HISTORY_DIR=`condor_config_val PER_JOB_HISTORY_DIR`
+safe_config_val HISTORY_DIR PER_JOB_HISTORY_DIR
 QUARANTINE_DIR=$HISTORY_DIR/quarantine
 [[ -d $QUARANTINE_DIR && -w $QUARANTINE_DIR ]] || fail "Cannot write to $QUARANTINE_DIR"
 
-HISTORY_EXTRA_ARGS=(-format "\n" EMPTY)
-safe_config_val SCALING_ATTR APEL_SCALING_ATTR
-[[ -z $SCALING_ATTR ]] || HISTORY_EXTRA_ARGS=(-format "%v|" "${SCALING_ATTR}" "${HISTORY_EXTRA_ARGS[@]}")
+CONDOR_Q_EXTRA_ARGS=(-format "\n" EMPTY)
+safe_ce_config_val SCALING_ATTR APEL_SCALING_ATTR
+[[ -z $SCALING_ATTR ]] || CONDOR_Q_EXTRA_ARGS=(-format "%v|" "${SCALING_ATTR}" "${CONDOR_Q_EXTRA_ARGS[@]}")
 
-safe_config_val BATCH_HOST APEL_BATCH_HOST
-safe_config_val CE_HOST APEL_CE_HOST
-safe_config_val CE_ID APEL_CE_ID
+safe_ce_config_val BATCH_HOST APEL_BATCH_HOST
+safe_ce_config_val CE_HOST APEL_CE_HOST
+safe_ce_config_val CE_ID APEL_CE_ID
 
 # Iterate over all the files in PER_JOB_HISTORY_DIR
-for file in $HISTORY_DIR/*
+for file in "$HISTORY_DIR"/*
 do
     # If $file is a directory, skip it
-    if [[ -d $file ]]; then
+    if [[ ! -f $file ]]; then
         continue
     fi
 
     # Check if $file is a valid history file by looking for a ClusterId value
     # If not found, assume the file is invalid and move to quarantine folder
-    if [[ -z "`condor_q -job $file -format \"%s\" ClusterId`" ]]; then
+    if [[ -z `condor_q -job $file -format \"%s\" ClusterId` ]]; then
         mv $file $QUARANTINE_DIR
         continue
     fi
@@ -56,7 +64,7 @@ do
         -format "%d|" ResidentSetSize_RAW \
         -format "%d|" ImageSize_RAW \
         -format "%d|" RequestCpus \
-        "${HISTORY_EXTRA_ARGS[@]}"`
+        "${CONDOR_Q_EXTRA_ARGS[@]}"`
     OUTPUT_FILE="$OUTPUT_DIR/batch-${OUTPUT_DATETIME}-$(hostname -s)"
     echo $batch_record >> $OUTPUT_FILE
 
