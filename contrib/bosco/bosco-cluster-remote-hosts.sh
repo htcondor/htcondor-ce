f5fa05abff0d96d00c3331732e5808b371d897b3
Brian Lin <blin@cs.wisc.edu>
2021-04-19 15:38:59 -0500

    Split out docs into its own branch

--- contrib/bosco/bosco-cluster-remote-hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source condor-ce environment
-[ -f /usr/share/condor-ce/condor_ce_env_bootstrap ] &&
-   . /usr/share/condor-ce/condor_ce_env_bootstrap
-
-export OVERRIDE_DIR=/etc/condor-ce/bosco_override
-/usr/share/condor-ce/bosco-cluster-remote-hosts.py
-
--- contrib/puppet/osg_ce_condor/manifests/params.pp
+++
@@ -1,6 +0,0 @@
-class osg_ce_condor::params {
-
-  $gums_server = 'osg-ce.domain.org'
-  $rms         = [ 'condor-ce-pbs', ]
-
-}
