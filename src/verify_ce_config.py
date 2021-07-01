d945fb2b6565301450b1983b61bf92c6d8bf258a
Mark Coatsworth <coatsworth@cs.wisc.edu>
2021-07-01 15:23:42 -0500

    Updated CE config verification to handle Job Router transforms (HTCONDOR-527)

--- src/verify_ce_config.py
+++ src/verify_ce_config.py
@@ -63,27 +63,29 @@ def main():
 
     # Create dict whose values are lists of ads specified in the relevant JOB_ROUTER_* variables
     parsed_jr_ads = defaultdict(list)
+    jr_transforms = {key : value for key, value in htcondor.param.items() if "JOB_ROUTER_ROUTE_" in key}
     for attr in ['JOB_ROUTER_DEFAULTS', 'JOB_ROUTER_ENTRIES']:
         try:
             config_val = htcondor.param[attr]
         except KeyError:
-            debug("Missing %s configuration value. " % attr +
-                  "Can be safely ignored if using 'JOB_ROUTER_ROUTE_*' ClassAd transforms.")
+            # Only raise an error here if no JOB_ROUTER_ROUTE_<name> configuration knobs are set
+            if not jr_transforms:
+                debug("Missing %s configuration value. " % attr)
             continue
 
         # store the ads (iterating through ClassAdStringIterator consumes them)
-        try:
-            parsed_jr_ads[attr] = list(classad.parseAds(config_val))
-        except ValueError:
-            # We shouldn't ever get here since classad.parseAds() only raises ValueError when it's given
-            # non-string/non-file output and htcondor.param shouldn't contain such values
-            debug("Failed to parse %s configuration value. " % attr +
-                  "Can be safely ignored if using 'JOB_ROUTER_ROUTE_*' ClassAd transforms.")
+        # We can ignore this for JOB_ROUTER_ENTRIES if there's at least one JOB_ROUTER_ROUTE_<name>
+        if attr != "JOB_ROUTER_ENTRIES" or not jr_transforms:
+            try:
+                parsed_jr_ads[attr] = list(classad.parseAds(config_val))
+            except ValueError:
+                # We shouldn't ever get here since classad.parseAds() only raises ValueError when it's given
+                # non-string/non-file output and htcondor.param shouldn't contain such values
+                debug("Failed to parse %s configuration value. " % attr)
 
-        # If JRD or JRE can't be parsed, the job router can't function
-        if not parsed_jr_ads[attr]:
-            debug("Could not read %s in the HTCondor-CE configuration. " % attr +
-                  "Can be safely ignored if using 'JOB_ROUTER_ROUTE_*' ClassAd transforms.")
+                # If JRD or JRE can't be parsed, the job router can't function
+                if not parsed_jr_ads[attr]:
+                    debug("Could not read %s in the HTCondor-CE configuration. " % attr)
 
         if attr == "JOB_ROUTER_ENTRIES":
             # Warn about routes we can find in the config that don't result in valid ads
@@ -103,35 +105,45 @@ def main():
                          "but cannot be found in JOB_ROUTER_ENTRIES: %s"
                          % ', '.join(missing_route_def))
 
-    # Find all eval_set_ attributes in the JOB_ROUTER_DEFAULTS
-    eval_set_defaults = set([x.lstrip('eval_') for x in parsed_jr_ads['JOB_ROUTER_DEFAULTS'][0].keys()
-                             if x.startswith('eval_set_')])
+    # Also warn about routes specified in JOB_ROUTER_ROUTE_<name> transforms that don't appear in the parsed JRE.
+    if jr_transforms:
+        for transform_attr, transform_value in jr_transforms:
+            missing_route_def = set(transform_value.replace(',',' ').split()).difference(set(parse_route_names(config_val)))
+            if missing_route_def:
+                warn("The following are specified in %s "
+                        "but cannot be found in JOB_ROUTER_ENTRIES: %s"
+                        % transform_attr, ', '.join(missing_route_def))
 
-    # Find all default_ attributes used in expressions in the JOB_ROUTER_DEFAULTS
-    default_attr = set([re.sub(r'.*(default_\w*).*', 'eval_set_\\1', str(x))
-                        for x in parsed_jr_ads['JOB_ROUTER_DEFAULTS'][0].values()
-                        if isinstance(x, classad.ExprTree) and "default_" in str(x)])
+    if parsed_jr_ads['JOB_ROUTER_DEFAULTS'] and parsed_jr_ads['JOB_ROUTER_ENTRIES']:
+        # Find all eval_set_ attributes in the JOB_ROUTER_DEFAULTS
+        eval_set_defaults = set([x.lstrip('eval_') for x in parsed_jr_ads['JOB_ROUTER_DEFAULTS'][0].keys()
+                                if x.startswith('eval_set_')])
 
-    for entry in parsed_jr_ads['JOB_ROUTER_ENTRIES']:
-        # Warn users if they've set_ attributes that would be overriden by eval_set in the JOB_ROUTER_DEFAULTS
-        overriden_attr = eval_set_defaults.intersection(set(entry.keys()))
-        if overriden_attr:
-            warn("%s in JOB_ROUTER_ENTRIES will be overriden by the JOB_ROUTER_DEFAULTS."
-                 % ', '.join(overriden_attr)
-                 + " Use the 'eval_set_' prefix instead.")
+        # Find all default_ attributes used in expressions in the JOB_ROUTER_DEFAULTS
+        default_attr = set([re.sub(r'.*(default_\w*).*', 'eval_set_\\1', str(x))
+                            for x in parsed_jr_ads['JOB_ROUTER_DEFAULTS'][0].values()
+                            if isinstance(x, classad.ExprTree) and "default_" in str(x)])
 
-        # Ensure that users don't set the job environment in the Job Router
-        if is_osg and any(x.endswith('environment') for x in entry.keys()):
-            error("Do not use the Job Router to set the environment. See documentation for more details: "
-                  + "https://htcondor.github.io/htcondor-ce/v5/configuration/writing-job-routes/#setting-job-environments")
+        for entry in parsed_jr_ads['JOB_ROUTER_ENTRIES']:
+            # Warn users if they've set_ attributes that would be overriden by eval_set in the JOB_ROUTER_DEFAULTS
+            overriden_attr = eval_set_defaults.intersection(set(entry.keys()))
+            if overriden_attr:
+                warn("%s in JOB_ROUTER_ENTRIES will be overriden by the JOB_ROUTER_DEFAULTS."
+                    % ', '.join(overriden_attr)
+                    + " Use the 'eval_set_' prefix instead.")
 
-        # Warn users about eval_set_ default attributes in the ENTRIES since their
-        # evaluation may occur after the eval_set_ expressions containg them in the
-        # JOB_ROUTER_DEFAULTS
-        no_effect_attr = default_attr.intersection(set([x for x in entry.keys() if x.startswith('eval_set_')]))
-        if no_effect_attr:
-            warn("%s in JOB_ROUTER_ENTRIES " % ', '.join(no_effect_attr)
-                 + "may not have any effect. Use the 'set_' prefix instead.")
+            # Ensure that users don't set the job environment in the Job Router
+            if is_osg and any(x.endswith('environment') for x in entry.keys()):
+                warn("Do not use the Job Router to set the environment. See documentation for more details: "
+                    + "https://htcondor.github.io/htcondor-ce/v5/configuration/writing-job-routes/#setting-job-environments")
+
+            # Warn users about eval_set_ default attributes in the ENTRIES since their
+            # evaluation may occur after the eval_set_ expressions containg them in the
+            # JOB_ROUTER_DEFAULTS
+            no_effect_attr = default_attr.intersection(set([x for x in entry.keys() if x.startswith('eval_set_')]))
+            if no_effect_attr:
+                warn("%s in JOB_ROUTER_ENTRIES " % ', '.join(no_effect_attr)
+                    + "may not have any effect. Use the 'set_' prefix instead.")
 
     # Warn users on OSG CEs if osg-configure has not been run
     if is_osg:
